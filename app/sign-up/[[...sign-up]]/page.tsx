a9f35c0f75568afe2ff1d5ae9247242d5df1f9dc
solomon-mh <solomon.muhye.wd@gmail.com>
2025-07-31 00:55:09 +0300

    Update SignUp component redirect URL to point to /subscribe

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="px-4 py-8 sm:py-12 lg:py-16 max-w-7xl mx-auto flex justify-center align-center">
-      <SignUp forceRedirectUrl="/create-profile" />
+      <SignUp forceRedirectUrl="/subscribe" />
     </div>
   );
 }
