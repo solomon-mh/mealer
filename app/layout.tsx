2fa0c6d75fc0103217e2eb33a2cbc5604924a272
solomon-mh <solomon.muhye.wd@gmail.com>
2025-07-31 00:55:03 +0300

    Add CreateProfileOnSignIn component to RootLayout for user profile handling

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import NavBar from "@/components/NavBar";
 import ReactQueryClientProvider from "@/components/react-query-client-provider";
+import CreateProfileOnSignIn from "@/components/create-profile";
 
 export const metadata: Metadata = {
   title: "AI Meal Plans | Simple SaaS Demo",
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body className="bg-gray-50 text-gray-900">
         <ReactQueryClientProvider>
           <ClerkProvider signInUrl="/login" signUpUrl="/sign-up">
+            <CreateProfileOnSignIn />
             <NavBar />
             {/* Main container for page content */}
             <main className="max-w-7xl mx-auto pt-16 p-4 min-h-screen">
