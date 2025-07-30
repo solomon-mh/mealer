a0797c17e0ab7be5ddcdb64bcba62f83959a50e5
solomon-mh <solomon.muhye.wd@gmail.com>
2025-07-30 05:27:57 +0300

    Wrap RootLayout component with ReactQueryClientProvider for improved data fetching

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import NavBar from "@/components/NavBar";
+import ReactQueryClientProvider from "@/components/react-query-client-provider";
 
 export const metadata: Metadata = {
   title: "AI Meal Plans | Simple SaaS Demo",
@@ -14,16 +15,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="bg-gray-50 text-gray-900">
-        <ClerkProvider>
-          <NavBar />
-          {/* Main container for page content */}
-          <main className="max-w-7xl mx-auto pt-16 p-4 min-h-screen">
-            {children}
-          </main>
-        </ClerkProvider>
-      </body>
-    </html>
+    <ReactQueryClientProvider>
+      <html lang="en">
+        <body className="bg-gray-50 text-gray-900">
+          <ClerkProvider>
+            <NavBar />
+            {/* Main container for page content */}
+            <main className="max-w-7xl mx-auto pt-16 p-4 min-h-screen">
+              {children}
+            </main>
+          </ClerkProvider>
+        </body>
+      </html>
+    </ReactQueryClientProvider>
   );
 }
