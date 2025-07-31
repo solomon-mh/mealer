528ab27a44212b843a5df50696deb6df2ade35a9
solomon-mh <solomon.muhye.wd@gmail.com>
2025-07-31 12:19:44 +0300

    Update SignUpPage to use search parameters for redirect URL after sign out

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,15 @@
-import { SignUp } from "@clerk/nextjs";
+"use client";
 
-export default function Page() {
+import { SignUp } from "@clerk/nextjs";
+import { useSearchParams } from "next/navigation";
+
+export default function SignUpPage() {
+  const searchParams = useSearchParams();
+  const redirectTo = searchParams.get("redirect_url") || "/";
+
   return (
     <div className="px-4 py-8 sm:py-12 lg:py-16 max-w-7xl mx-auto flex justify-center align-center">
-      <SignUp forceRedirectUrl="/subscribe" />
+      <SignUp forceRedirectUrl="/subscribe" afterSignOutUrl={redirectTo} />
     </div>
   );
 }
