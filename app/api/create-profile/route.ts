f23f6fc73b6f5ce7447234faa6a25515288deb27
solomon-mh <solomon.muhye.wd@gmail.com>
2025-07-31 16:03:44 +0300

    Refactor create-profile API to streamline user authentication and profile creation logic

--- app/api/create-profile/route.ts
+++ app/api/create-profile/route.ts
@@ -1,46 +1,43 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
-import { auth, currentUser } from "@clerk/nextjs/server";
+import { auth } from "@clerk/nextjs/server";
 
 export async function POST() {
   try {
     const { userId } = await auth();
+
     if (!userId) {
       return NextResponse.json(
-        { error: "User not found in Clerk." },
-        { status: 404 }
-      );
-    }
-
-    const clerkUser = await currentUser();
-    if (!clerkUser) {
-      return NextResponse.json(
-        { error: "User not found in Clerk." },
-        { status: 404 }
-      );
-    }
-    const email = clerkUser.emailAddresses?.[0]?.emailAddress || "";
-    if (!email) {
-      return NextResponse.json(
-        { error: "User does not have an email address." },
-        { status: 400 }
+        { error: "User not authenticated." },
+        { status: 401 }
       );
     }
 
     // Check if profile already exists
-    const existingProfile = await prisma.profile?.findUnique({
-      where: { userId: clerkUser.id },
+    const existingProfile = await prisma.profile.findUnique({
+      where: { userId },
     });
 
     if (existingProfile) {
-      // Profile already exists
       return NextResponse.json({ message: "Profile already exists." });
     }
 
-    // Otherwise, create the profile
+    const userResponse = await fetch(
+      `https://api.clerk.com/v1/users/${userId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.CLERK_SECRET_KEY}`,
+        },
+      }
+    );
+    const clerkUser = await userResponse.json();
+
+    const email = clerkUser.email_addresses?.[0]?.email_address || "";
+
+    // Create profile
     await prisma.profile.create({
       data: {
-        userId: clerkUser.id,
+        userId,
         email,
         subscriptionActive: false,
         subscriptionTier: null,
@@ -48,12 +45,11 @@ export async function POST() {
       },
     });
 
-    console.log(`Prisma profile created for user: ${clerkUser.id}`);
     return NextResponse.json(
       { message: "Profile created successfully." },
       { status: 201 }
     );
-  } catch (error: unknown) {
+  } catch (error) {
     console.error("Error in create-profile API:", error);
     return NextResponse.json(
       { error: "Internal Server Error." },
