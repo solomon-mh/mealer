e739c551343813422407d93168a1e61684d34a22
solomon-mh <solomon.muhye.wd@gmail.com>
2025-07-31 15:45:12 +0300

    Refactor SignUpPage to remove hardcoded redirect URL and use dynamic redirect from search parameters

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -9,7 +9,7 @@ export default function SignUpPage() {
 
   return (
     <div className="px-4 py-8 sm:py-12 lg:py-16 max-w-7xl mx-auto flex justify-center align-center">
-      <SignUp forceRedirectUrl="/subscribe" afterSignOutUrl={redirectTo} />
+      <SignUp forceRedirectUrl={redirectTo} />
     </div>
   );
 }
