6505f343b30eae85d9a10a4017874ac816e0accf
solomon-mh <solomon.muhye.wd@gmail.com>
2025-07-31 12:20:16 +0300

    Refactor middleware to improve route matching and enhance subscription handling logic

--- middleware.ts
+++ middleware.ts
@@ -4,19 +4,51 @@ import { NextResponse } from "next/server";
 const isPublicRoute = createRouteMatcher([
   "/",
   "/sign-up(.*)",
-  "subscribe(.*)",
+  "/subscribe(.*)",
+  "/api/webhook(.*)",
+  "/api/checkout(.*)",
+  "/api/stripe-webhook(.*)",
+  "/api/check-subscription(.*)",
 ]);
 const isSignUpRoute = createRouteMatcher(["/sign-up(.*)"]);
+const isMealPlanRoute = createRouteMatcher(["/mealplan(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
   const userAth = await auth();
   const { userId } = userAth;
-  const { origin } = req.nextUrl;
+  const { pathname, origin } = req.nextUrl;
+
+  if (pathname === "/api/check-subscription") {
+    return NextResponse.next();
+  }
   if (!isPublicRoute(req) && !userId) {
-    return NextResponse.redirect(new URL("/sign-up", origin));
+    const redirectUrl = new URL("/sign-up", origin);
+    const destination = req.nextUrl.pathname + req.nextUrl.search;
+    redirectUrl.searchParams.set("redirect_url", destination);
+    return NextResponse.redirect(redirectUrl);
   }
   if (isSignUpRoute(req) && userId) {
     return NextResponse.redirect(new URL("/mealplan", origin));
+  }
+
+  if (isMealPlanRoute(req) && userId) {
+    try {
+      const response = await fetch(
+        `${origin}/api/check-subscription?userId=${userId}`,
+        { method: "GET", headers: { "x-middleware-bypass": "true" } }
+      );
+      if (!response.ok) {
+        throw new Error("API returned an error");
+      }
+      const data = await response.json();
+      if (!data.subscriptionActive) {
+        return NextResponse.redirect(new URL("/subscribe", origin));
+      }
+    } catch (error) {
+      console.log(error);
+
+      return NextResponse.redirect(new URL("/subscribe", origin));
+    }
   }
 
   return NextResponse.next();
